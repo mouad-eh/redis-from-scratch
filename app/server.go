441d5f1bc6b3485f99d0affc4c1154a7b352a10f
mouad-eh <jm_elhaouari@esi.dz>
2023-07-09 19:29:43 +0100

    stage 1 fix

--- app/server.go
+++ app/server.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "172.0.0.1:6379")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
